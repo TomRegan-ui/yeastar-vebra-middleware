19b42cd1e0b8757bd22b878b143edc99da14c95e
TomRegan-ui <tr@better.co>
2026-05-22 14:55:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+import express from "express";
+
 app.get("/debug", (req, res) => {
   res.send("NEW VERSION LIVE
 ✅");
 });
 ``
-import express from "express";
-
 const app = express();
 app.use(express.json());
 
